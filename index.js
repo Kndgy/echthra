5c387f609be15813d30246eabe6ed609654a243e
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-05 16:56:17 +0700

    add automatic message

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ client.on('ready', () => {
   console.log(`${client.user.username} is up and running!`);
 })
 
+bot.setInterval(function(){
+  var generalChannel = bot.channels.get("723794736325853209"); // Replace with known channel ID
+  generalChannel.send("Hello, world!") ;
+}, 10000);
+
 
 client.on("message", message => {
   if (message.author.bot) return;
