240c95377bc6ca27a3f6a5287a961df8d8cb1309
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-12 19:58:03 +0700

    RACOOOON

---
+++ commands/raccoon.js
@@ -0,0 +1,15 @@
+var pixabay = require('pixabay-api')
+var key = "20235915-ae4a7fd433d4ee7b7823ec7b5"
+const {MessageEmbed} = require ('discord.js');
+
+module.exports = {
+    name: 'raccoon',
+    description: 'racoon',
+    async execute(message, args){
+        var r = await pixabay.searchImages(key, 'raccoon')
+        const embed = new MessageEmbed()
+        .setTitle("RACCOOOOOOOOOON")
+        .setImage(r.hits[Math.floor(Math.random()*r.hits.length)].largeImageURL)
+        message.channel.send(embed);
+    }
+}
