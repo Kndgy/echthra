bb4dc8dbb8e8baea533c3957df1de4cc91dc9d61
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-14 10:23:25 +0700

    bingguss

--- commands/bingus.js
+++ commands/bingus.js
@@ -2,8 +2,15 @@ const bingus =["https://media.discordapp.net/attachments/748780918067691582/8103
  "https://media.discordapp.net/attachments/748780918067691582/810331982743076884/Hi_Bingus.png",
 "https://media.discordapp.net/attachments/748780918067691582/810332004439556116/EovwM_CWMAEB_BZ.jpg?width=516&height=552",
 "https://media.discordapp.net/attachments/748780918067691582/810332015060451328/l58auzpyw5p51.jpg?width=589&height=551",
-"https://media.discordapp.net/attachments/748780918067691582/810332024577196053/gvzyqWI.jpg?width=457&height=552"]
-
+"https://media.discordapp.net/attachments/748780918067691582/810332024577196053/gvzyqWI.jpg?width=457&height=552",
+"https://media.discordapp.net/attachments/775500327079903252/810349102097104897/image0.png?width=489&height=552",
+"https://media.discordapp.net/attachments/775500327079903252/810349102357282816/image1.png?width=573&height=552",
+"https://cdn.discordapp.com/attachments/775500327079903252/810349102659010580/image2.png",
+"https://media.discordapp.net/attachments/775500327079903252/810348912690855946/image0.png",
+"https://media.discordapp.net/attachments/775500327079903252/810349855238389780/image0.png?width=572&height=552",
+"https://media.discordapp.net/attachments/775500327079903252/810350131386908712/image0.png?width=581&height=552",
+"https://media.discordapp.net/attachments/775500327079903252/810350131941474324/image1.png?width=584&height=552"
+]
 module.exports = {
     name : 'bingus',
     description: 'bingus',
