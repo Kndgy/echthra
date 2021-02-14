829d842f5661f886091cf6174493c0e6448adc93
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-15 01:45:40 +0700

    foods

--- commands/food.js
+++ commands/food.js
@@ -7,17 +7,16 @@ module.exports = {
     description:'random food picts',
     async execute(message, args){
         try {
-            const { body } = await fetch('https://www.reddit.com/r/FoodPorn.json?sort=top&t=week')
+            const { file } = await fetch('https://www.reddit.com/r/FoodPorn.json?sort=top&t=week')
                 
-            const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-            if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-            const randomnumber = Math.floor(Math.random() * allowed.length)
+            if (!file.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+            const randomnumber = Math.floor(Math.random() * file.length)
             const embed = new MessageEmbed()
             .setColor(0xffabd7)
-            .setTitle(allowed[randomnumber].data.title)
-            .setDescription("Posted by: " + allowed[randomnumber].data.author)
-            .setImage(allowed[randomnumber].data.url)
-            .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+            .setTitle(file[randomnumber].data.title)
+            .setDescription("Posted by: " + file[randomnumber].data.author)
+            .setImage(file[randomnumber].data.url)
+            .addField("Other info:", "Up votes: " + file[randomnumber].data.ups + " / Comments: " + file[randomnumber].data.num_comments)
             .setFooter("Memes provided by r/dankmemes")
             message.channel.send(embed)
         } catch(err){
