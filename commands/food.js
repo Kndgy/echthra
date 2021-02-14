bef7af9e041ea0130bd4544eeb987c54b331f08d
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-15 02:01:47 +0700

    foods

--- commands/food.js
+++ commands/food.js
@@ -1,24 +1,29 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
+const reddit = require('@elchologamer/random-reddit');
 
 module.exports = {
     name:'food',
     description:'random food picts',
-    async execute(message, args){
-        try {
-            const { file } = await fetch('https://www.reddit.com/r/FoodPorn.json?sort=top&t=week')
+    execute(message, args){
+        
+
+        let options = {
+           imageOnly: true,
+           allowNSFW: true
+        };
+        
+        reddit.getPost('https://www.reddit.com/r/FoodPorn/', options).then(post => { 
             
-            const random = Math.floor(Math.random() * file)
-            const embed = new MessageEmbed()
-            .setColor(0xffabd7)
-            .setTitle(file[random].data.title)
-            .setDescription("Posted by: " + file[random].data.author)
-            .setImage(file[random].data.url)
-            .addField("Up votes: " + file[random].data.ups + " / Comments: " + file[random].data.num_comments)
-            .setFooter("Memes provided by r/dankmemes")
-            message.channel.send(embed)
-        } catch(err){
-            return console.log(err);
-        }
+           var title = post.title
+           var content = post.text
+           var postURL = post.permalink
+           var postAuthor = post.author
+           var upvotes = post.upvotes
+           var downvotes = post.downvots
+
+           message.channel.send(post);
+        })
+        
     }
 }
