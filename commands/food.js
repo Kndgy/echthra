53ea7b5a4d89aa1205d2c4a373e111cffad22c97
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-15 01:47:08 +0700

    foods

--- commands/food.js
+++ commands/food.js
@@ -8,15 +8,14 @@ module.exports = {
     async execute(message, args){
         try {
             const { file } = await fetch('https://www.reddit.com/r/FoodPorn.json?sort=top&t=week')
-                
-            if (!file.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
+            
             const randomnumber = Math.floor(Math.random() * file.length)
             const embed = new MessageEmbed()
             .setColor(0xffabd7)
             .setTitle(file[randomnumber].data.title)
             .setDescription("Posted by: " + file[randomnumber].data.author)
             .setImage(file[randomnumber].data.url)
-            .addField("Other info:", "Up votes: " + file[randomnumber].data.ups + " / Comments: " + file[randomnumber].data.num_comments)
+            .addField("Up votes: " + file[randomnumber].data.ups + " / Comments: " + file[randomnumber].data.num_comments)
             .setFooter("Memes provided by r/dankmemes")
             message.channel.send(embed)
         } catch(err){
