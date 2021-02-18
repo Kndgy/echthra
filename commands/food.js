f25d3a59105aa3ff7cc54b28933b5d140f896fa3
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-18 16:09:22 +0700

    test

--- commands/food.js
+++ commands/food.js
@@ -4,14 +4,12 @@ module.exports = {
     name: 'food',
     description: 'fooods',
     async execute(message, args){
-        try {
+     
             const { body } = await fetch('https://www.reddit.com/r/foodporn.json?sort=top&t=week').then(
                 response => response.json());
               
             
             message.channel.send(body);
-        } catch (err) {
-            return console.log(err);
-        }
+        
     }
 }
