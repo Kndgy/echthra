7d3f11dc4623dd599e59dafbafee3eecab700d61
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-18 16:05:32 +0700

    test

---
+++ commands/food.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+
+module.exports = {
+    name: 'food',
+    description: 'fooods',
+    async execute(message, args){
+        try {
+            const { body } = await fetch('https://www.reddit.com/r/foodporn.json?sort=top&t=week').then(
+                response => response.json())
+              
+            
+            message.channel.send(body)
+        } catch (err) {
+            return console.log(err);
+        }
+    }
+}
