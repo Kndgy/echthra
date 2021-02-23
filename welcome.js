6a6d501bc6e5be78c3d7437a8f3d3a60098c5186
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-23 20:05:51 +0700

    add temp welcome message for a private server

--- welcome.js
+++ welcome.js
@@ -1,11 +1,23 @@
 module.exports = (client) => {
-    const channelId = "801664083342393376";
+    const channelId = '813004160617283586';
     client.on("guildMemberAdd", (member) =>{
         console.log(member);
 
-        const message = `welcome <@${member.id}> bruh`
+        const message = [
+            `Hello <@${member.id}>`,
+            "",
+            "Thank you for Joining Yasuragi support server. ",
+            "I'm sorry that our bot has detected your account as a potential alt account.",
+            "",
+            "Voice verification with our moderator is required for you to access Yasuragi main server.",
+            "If you're ready to start the verification, you can ping @Admin , and the currently available admin will reach out to you. ",
+            "After you passed the verification process, you will be granted access to our Main server.",
+            "",
+            "*This message is automatically generated*",
+        ]
+        
 
-        const channel = member.guild.channels.cache(channelId);
+        const channel = member.guild.channels.cache.get(channelId);
         channel.send(message)
     });
 };
