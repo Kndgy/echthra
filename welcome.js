303d4e118d863d71dc66bbfeabe4084242be5156
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-23 23:30:55 +0700

    test temp welcome message

--- welcome.js
+++ welcome.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = (client) => {
     client.on("guildMemberAdd", (member) =>{
 
-        var channel = member.guild.channels.cache.get('813004160617283586');
+        var channel = member.guild.channels.cache.get('801664083342393376');
         const embed = new MessageEmbed()
         .setColor(0xffabd7)
         .setAuthor('Yasuragi Support Server')
@@ -11,13 +11,7 @@ module.exports = (client) => {
             {name: ' ',  value: `Hello <@${member.id}>`},
             {name:' ',value:" " },
             {name:' ',value: "Thank you for Joining Yasuragi support server. "},
-            {value:  "I'm sorry that our bot has detected your account as a potential alt account."},
-            {value: "" },
-            {value: "Voice verification with our moderator is required for you to access Yasuragi main server."},
-            {value: `If you're ready to start the verification, you can ping <@&813712228183834655> in <#813003820518998039>, and the currently available admin will reach out to you.`},
-            {value: "After you passed the verification process, you will be granted access to our Main server."},
-            {value: ""},
-            {value: "*This message is automatically generated*"},
+            {name:' ',value:  "I'm sorry that our bot has detected your account as a potential alt account."}
         )
         .setTimestamp()
         channel.send(embed)
