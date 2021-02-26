3fa5073e8b6ba1aa9882f982f156ee921b038dc2
Rafli Ghani <ghanirafli8@gmail.com>
2021-02-27 01:12:31 +0700

    info

--- commands/info.js
+++ commands/info.js
@@ -7,8 +7,8 @@ module.exports = {
         message.channel.send(`**Fumiko Discord Bot**
 This bot mainly just for fun.(supposedly exclusive for a server)
 Use 'help command to see more what fumiko can do
-Fumiko is open source and currently being developed by \`Kan#9015\`
+Fumiko is open source and currently being developed by \`Kan#9015\` feel free to dm me if you have any questions about fumiko or want to suggest ideas.
 if you would like to contribute and make changes, go ahead to (https://github.com/Kndgy/echo) feel free to open issues or/and pr.
-feel free to dm me if you have any questions about fumiko or want to suggest ideas. A side project i maintain to learn more about node.js`)
+A side project i maintain to learn more about node.js`)
     }
 }
